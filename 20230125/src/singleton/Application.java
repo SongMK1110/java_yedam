f36b7f8dc28bb4d6872e2534b6318f8583a8554e
Minkyu Song <thdalsrb123@naver.com>
2023-01-25 17:49:50 +0900

    0125

---
+++ 20230125/src/singleton/Application.java
@@ -0,0 +1,12 @@
+package singleton;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 싱글톤
+		// 프로그램 내에서 단 하나의 객체를 보장
+		// java <-> db 연결
+		// springframe에서 사용
+	}
+
+}
